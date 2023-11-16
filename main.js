fbedc77ab4442d72e2ccf91e297220fc883ce4d6
mahmoudsharkawey <ma7moudelsharkawey@gmail.com>
2023-11-16 21:19:05 +0200

    timer

--- main.js
+++ main.js
@@ -3,38 +3,43 @@
           minute = second * 60,
           hour = minute * 60,
           day = hour * 24;
-  
-    const targetDate = new Date();
-    targetDate.setDate(targetDate.getDate() + 25);
-  
+
+    let targetDate = localStorage.getItem('targetDate');
+    if (!targetDate) {
+        targetDate = new Date();
+        targetDate.setDate(targetDate.getDate() + 25);
+        localStorage.setItem('targetDate', targetDate);
+    } else {
+        targetDate = new Date(targetDate);
+    }
+
     const calculateTimeRemaining = () => {
-      const now = new Date();
-      const difference = targetDate - now;
-      const days = Math.floor(difference / day);
-      const hours = Math.floor((difference % day) / hour);
-      const minutes = Math.floor((difference % hour) / minute);
-      const seconds = Math.floor((difference % minute) / second);
-  
-      return { days, hours, minutes, seconds };
+        const now = new Date();
+        const difference = targetDate - now;
+        const days = Math.floor(difference / day);
+        const hours = Math.floor((difference % day) / hour);
+        const minutes = Math.floor((difference % hour) / minute);
+        const seconds = Math.floor((difference % minute) / second);
+
+        return { days, hours, minutes, seconds };
     };
-  
+
     const updateCountdown = () => {
-      const timeRemaining = calculateTimeRemaining();
-  
-      document.getElementById("days").innerText = timeRemaining.days;
-      document.getElementById("hours").innerText = timeRemaining.hours;
-      document.getElementById("minutes").innerText = timeRemaining.minutes;
-      document.getElementById("seconds").innerText = timeRemaining.seconds;
+        const timeRemaining = calculateTimeRemaining();
+
+        document.getElementById("days").innerText = timeRemaining.days;
+        document.getElementById("hours").innerText = timeRemaining.hours;
+        document.getElementById("minutes").innerText = timeRemaining.minutes;
+        document.getElementById("seconds").innerText = timeRemaining.seconds;
     };
-  
+
     const intervalId = setInterval(updateCountdown, 1000);
-  
+
     // Optional: Uncomment this block if you want to stop the countdown after 20 days
     /*
     setTimeout(() => {
-      clearInterval(intervalId);
-      document.getElementById("countdown").innerHTML = "<p>Countdown expired!</p>";
+        clearInterval(intervalId);
+        document.getElementById("countdown").innerHTML = "<p>Countdown expired!</p>";
     }, 20 * day);
     */
-  })();
-  
+})();
